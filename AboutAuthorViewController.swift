b6e6e90607b5b1b1bc00e1d7b258e71141be0e2a
Jacob Olenick <olenickjacob@gmail.com>
2019-05-30 14:34:24 -0700

    View Controller code for Swift
    Uploaded code from Bull's Eye Game for iOS

---
+++ AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEyeGame
+//
+//  Created by Jacob Olenick on 5/29/19.
+//  Copyright © 2019 Jacob Olenick. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  BullsEyeGame
+//
+//  Created by Jacob Olenick on 5/29/19.
+//  Copyright © 2019 Jacob Olenick. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
